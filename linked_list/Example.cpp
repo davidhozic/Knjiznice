caae182717740eb2afb4d661e9f88f78f1b9018c
davidhozic <davidhozic@gmail.com>
2021-07-08 09:04:42 +0200

    Updated example

--- linked_list/Example.cpp
+++ linked_list/Example.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 
 #include "linked_list.hpp"
+#include <vector>
+
 
 
 using namespace std;
@@ -27,16 +29,29 @@ int compare (PERSON_t a , PERSON_t b)
 }
 
 
+/* USE CLONE to copy the list, it does not do it automaticaly */
+void test(LIST_t <PERSON_t> old_object)
+{
+    LIST_t <PERSON_t> new_object;
+    new_object.clone(old_object);
+}
+
+
 LIST_t <PERSON_t> people;
+
+
+
 int main()
 {
-    people = PERSON_t("Lucifer", 50)  +   people;
-    people = PERSON_t("David", 5000)  +   people;
-    people = PERSON_t("Britney", 100) +   people;
-    people = PERSON_t("Amanda", 1232) +   people;
-    people = PERSON_t("Teja", 10000)  +   people;
-    people = people + PERSON_t("Johny", 100000);
+    
 
+        people = PERSON_t("Lucifer", 50)  +   people;
+        people = PERSON_t("David", 5000)  +   people;
+        people = PERSON_t("Britney", 100) +   people;
+        people = PERSON_t("Amanda", 1232) +   people;
+        people = PERSON_t("Teja", 10000)  +   people;
+        people = people + PERSON_t("Johny", 100000);
+    
 
     cout << "Before sort: "
          << endl;
@@ -49,8 +64,9 @@ int main()
           << endl;
 
     for (uint32_t i = 0, l = people.length(); i < l; i++)
-        cout << people[i].name << endl;
-    
+        cout << people[i].name << endl; 
+
+   
 
     return 0;
 }
